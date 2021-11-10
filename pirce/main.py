b1bfce19b8518c6a4719dd97ce77aed5e21d15a0
keeYuc <52500690@qq.com>
2021-11-10 16:01:40 +0800

    update

--- pirce/main.py
+++ pirce/main.py
@@ -89,8 +89,6 @@ class bk_updater():
         path = '操作记录.xlsx'
         wb = openpyxl.load_workbook(path)
         ws = wb['板块强度']
-        wb['{}'.format(time.strftime(
-            "%Y-%m-%d %H:%M:%S", time.localtime()))] = copy.deepcopy(ws)
         start = 7
         for k in self.f_map:
             ws['A{}'.format(start)] = k
