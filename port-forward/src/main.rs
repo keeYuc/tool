e69d42c8ad7f9b953fa2dddec4523d5c1771402f
keeYuc <52500690@qq.com>
2021-11-19 15:38:15 +0800

    add code

---
+++ port-forward/src/main.rs
@@ -0,0 +1,51 @@
+extern crate clap;
+use clap::{App, Arg};
+use std::io::{self, Read};
+use std::process::Command;
+use std::str;
+
+fn do_command(n: String, c: String) {
+    let pod_command = format!("kubectl get pod -n {}", n);
+    let a = Command::new("bash")
+        .arg("-c")
+        .arg(pod_command)
+        .output()
+        .expect("failed to execute process")
+        .stdout;
+    let a: Vec<&str> = str::from_utf8(&a).unwrap().split('\n').collect();
+    //let a=String::from_utf8(a).unwrap().split('\n').collect();
+    println!("{:?}", a)
+}
+
+fn main() {
+    let matches = App::new("port-forward")
+        .version("1.0")
+        .author("Keeyu<keeyucc@gmail.com>")
+        .about("be happy")
+        .arg(
+            Arg::with_name("namespace")
+                .short("n")
+                .long("namespace")
+                .value_name("NAMESPACE")
+                .help("Sets a namespace for command")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("container")
+                .short("c")
+                .long("container")
+                .value_name("CONTAINER")
+                .help("Sets a container for command")
+                .takes_value(true),
+        )
+        .get_matches();
+    let mut n = "gcp".to_string();
+    let mut c = "merchant".to_string();
+    if let Some(s) = matches.value_of("namespace") {
+        n = String::from(s)
+    }
+    if let Some(s) = matches.value_of("container") {
+        c = String::from(s)
+    }
+    do_command(n, c);
+}
