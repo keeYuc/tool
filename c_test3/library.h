39a9541b80df949ea6f57f85d7e8082024043f1c
keeYuc <52500690@qq.com>
2021-12-10 12:02:50 +0800

    update

---
+++ c_test3/library.h
@@ -0,0 +1,6 @@
+#ifndef C_TEST3_LIBRARY_H
+#define C_TEST3_LIBRARY_H
+
+void hello();
+
+#endif //C_TEST3_LIBRARY_H
--- test1/src/main.rs
+++ test1/src/main.rs
@@ -1,16 +1,3 @@
 fn main() {
-    let c = bb::XX(aa {
-        name: String::from("aa"),
-    });
-  
-    
-    
-}
-
-struct aa {
-    name: String,
-}
-
-enum bb {
-    XX(aa),
+    println!("hello")
 }
