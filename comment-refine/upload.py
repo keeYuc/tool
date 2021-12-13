43ea0f1f71ecb9d52ba30d6904e1ec45a458daa6
keeYuc <52500690@qq.com>
2021-12-13 10:56:24 +0800

    update

--- comment-refine/upload.py
+++ comment-refine/upload.py
@@ -1,12 +1,14 @@
-
+import grpc
 from protocol.file_center import file_center_service_pb2_grpc
 from protocol.file_center import image_pb2
-rpc_url = 'file-center.regoo:9000'
-
-with open('./564792.jpg') as f:
+rpc_url = 'localhost:9007'
+print("yes")
+con = grpc.insecure_channel(rpc_url)
+print("yes")
+server = file_center_service_pb2_grpc.FileCenterServiceStub(con)
+print("yes")
+print(server)
+with open('./564792.jpg', 'rb') as f:
     s = f.read()
-    server = file_center_service_pb2_grpc.FileCenterServiceStub(
-        grpc.insecure_channel(rpc_url))
-    rsb = server.UploadImage(image_pb2.UploadImageReq(
-        AppId="GCP", CloseImgCompress="false", ImgContent=s))
+    rsb = server.UploadImage(image_pb2.UploadImageReq(app_id="GCP", close_img_compress="false"))
     print(rsb)
