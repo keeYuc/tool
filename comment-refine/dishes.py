e896354fc0389026303e33bd049f279850591902
keeYuc <52500690@qq.com>
2021-12-14 11:57:42 +0800

    update

--- comment-refine/dishes.py
+++ comment-refine/dishes.py
@@ -1,8 +1,11 @@
 import os
-#import grpc
+import grpc
+import time
+import random
+import pymongo
 import dishes_config
-#from protocol.file_center import file_center_service_pb2_grpc
-#from protocol.file_center import image_pb2
+from protocol.file_center import file_center_service_pb2_grpc
+from protocol.file_center import image_pb2
 
 rpc_url_fct = 'localhost:9007'
 url = 'mongodb://root:8DNsidknweoRGwSbWgDN@localhost:27019'
@@ -11,6 +14,7 @@ database = "content"
 
 class dishes():
     def __init__(self):
+        # pass
         myclient = pymongo.MongoClient(url)
         self.table_dishes_img = myclient[database]["dishes_img"]
         self.table_shop = myclient[database]["shop"]
@@ -28,25 +32,59 @@ class dishes():
     def img_import(self):
         con = grpc.insecure_channel(rpc_url_fct)
         server = file_center_service_pb2_grpc.FileCenterServiceStub(con)
+        self.url_map = {}
         for k in self.files:
             with open(self.files[k], 'rb') as f:
                 s = f.read()
                 rsb = server.UploadImage(image_pb2.UploadImageReq(
                     app_id="1", close_img_compress="false", img_content=s))
-                url_map.url_map[k] = rsb['url']
-                self.table_dishes_img.insert_one({'name': k, 'url': rsb['url']})
+
+                self.url_map[k] = rsb.url
+                self.table_dishes_img.insert_one(
+                    {'name': k, 'url': rsb.url, 'create_at': int(time.time())})
         print('dishes import fin')
 
-    def dishes_build(self):
-        for shop_id in dishes_config.shop_ids:
-            pass
-
     def dishes_import(self):
-        pass
+        sum_ = 0
+        for shop_id in dishes_config.shop_ids:
+            shop = self.table_shop.find_one({'shop_id': shop_id})
+            try:
+                rand_list = self.__rand_list(shop['tag']['all'])
+                shop_dishes = []
+                for _ in random.randint(5, 15):
+                    shop_dishes.append(
+                        self.tag_dishes[self.choice_one(rand_list)])
+                    sum_ += 1
+                # todo 调用更新接口更新shop
+            except:
+                pass
+
+    def __rand_list(self, list_: list):
+        rand_map = []
+        for id_ in list_:
+            for _ in random.randint(0, 19):
+                rand_map.append(id_)
+        return rand_map
+
+    def choice_one(self, list: list):
+        return list[[random.randint(0, len(list)-1), random.randint(0,
+                                                                    len(list)-1), random.randint(0, len(list)-1)][random.randint(0, 2)]]
+
+    def dishes_build(self):
+        self.tag_dishes = {}
+        for i in dishes_config.tag_map:
+            img = self.table_dishes_img.find_one({'name': i[2]})
+            if i[0] not in self.tag_dishes.keys():
+                self.tag_dishes[i[0]] = [
+                    {'dishes_name': i[1], 'image':img['url']}]
+            else:
+                self.tag_dishes[i[0]].append(
+                    {'dishes_name': i[1], 'image': img['url']})
+        print('dishes build fin')
 
     def insert_dishes_shop(self, shop_id, dishes):
         pass
 
 
 if __name__ == '__main__':
-    dishes().img_import()
+    dishes().dishes_build()
--- comment-refine/dishes_config.py
+++ comment-refine/dishes_config.py
@@ -1,1 +1,5 @@
 shop_ids = []
+tag_map = [('5adad7d516d74c5f81411016830cce30',		'Piyaz','Piyaz'),
+           ('5adad7d516d74c5f81411016830cce30',
+            'Ezogelin corba',	'Ezogelin corbası'),
+           ('2e60f73d963643fe8206f7831a2fc149',		'Piyaz', 'Piyaz')]
