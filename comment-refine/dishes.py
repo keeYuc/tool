e69adf951cbc9db0ddb0a0760c6e0ec65b5f9593
keeYuc <52500690@qq.com>
2021-12-14 10:37:04 +0800

    update

--- comment-refine/dishes.py
+++ comment-refine/dishes.py
@@ -1,8 +1,8 @@
 import os
-import grpc
+#import grpc
 import dishes_config
-from protocol.file_center import file_center_service_pb2_grpc
-from protocol.file_center import image_pb2
+#from protocol.file_center import file_center_service_pb2_grpc
+#from protocol.file_center import image_pb2
 
 rpc_url_fct = 'localhost:9007'
 url = 'mongodb://root:8DNsidknweoRGwSbWgDN@localhost:27019'
@@ -12,7 +12,7 @@ database = "content"
 class dishes():
     def __init__(self):
         myclient = pymongo.MongoClient(url)
-        self.table_dishes = myclient[database]["dishes"]
+        self.table_dishes_img = myclient[database]["dishes_img"]
         self.table_shop = myclient[database]["shop"]
         self.files = {}
         self.prefix = '.'
@@ -28,14 +28,13 @@ class dishes():
     def img_import(self):
         con = grpc.insecure_channel(rpc_url_fct)
         server = file_center_service_pb2_grpc.FileCenterServiceStub(con)
-        self.url_map = {}
         for k in self.files:
             with open(self.files[k], 'rb') as f:
                 s = f.read()
                 rsb = server.UploadImage(image_pb2.UploadImageReq(
                     app_id="1", close_img_compress="false", img_content=s))
                 url_map.url_map[k] = rsb['url']
-                self.table_dishes.insert_one({'name': k, 'url': rsb['url']})
+                self.table_dishes_img.insert_one({'name': k, 'url': rsb['url']})
         print('dishes import fin')
 
     def dishes_build(self):
