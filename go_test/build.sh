60482c89daab9e35f6924787bf74d2d13dcc357f
keeYuc <52500690@qq.com>
2021-12-31 15:35:55 +0800

    ad

--- go_test/build.sh
+++ go_test/build.sh
@@ -1,1 +1,3 @@
-CGO_ENABLED=1 GOOS=windows GOARCH=386  go build -buildmode=c-shared -o main.dll main.go
+CGO_ENABLED=1 GOOS=windows GOARCH=386 go build -buildmode=c-archive -o main.lib main.go
+#CGO_ENABLED=1 GOOS=windows GOARCH=386 go build -buildmode=c-shared -o main.so main.go
+#CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -buildmode=c-shared -o main.a main.go
