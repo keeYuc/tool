b7d42307cd12e684aef4b8724ac47ddc362b4c66
keeYuc <52500690@qq.com>
2022-01-04 20:24:03 +0800

    update

--- plugin/analyzer/fx.go
+++ plugin/analyzer/fx.go
@@ -1,1 +1,22 @@
 package analyzer
+
+import "app/model"
+
+func Contain(arr []*model.Pure) []*model.Pure {
+	long := len(arr)
+	arr_new := make([]*model.Pure, 0, long)
+	cache := new(model.Pure)
+	for i := 0; i-1 < long; i++ {
+		if arr[i].Contain(arr[i+1]) {
+			//todo 包含
+		} else if cache.H+cache.C+cache.L != 0 {
+			arr_new = append(arr_new, cache)
+			//todo
+		} else {
+			// todo
+		}
+	}
+	return arr_new
+}
+
+func Fx(arr []*model.Pure) {}
--- plugin/model/tdx.go
+++ plugin/model/tdx.go
@@ -1,5 +1,21 @@
 package model
 
+type Pure struct {
+	C     float32
+	H     float32
+	L     float32
+	Range IndexRange
+}
+
+type IndexRange struct {
+	L int
+	R int
+}
+
+func (p *Pure) Contain(next *Pure) bool {
+	return p.H >= next.H && p.L <= next.H
+}
+
 type Tdx struct {
 	Len int
 	InA []float32
@@ -14,4 +30,17 @@ func Exchange(dataLen int, a, b, c []float32) Tdx {
 		InB: b,
 		InC: c,
 	}
+}
+
+func ExchangePure(dataLen int, a, b, c []float32) []*Pure {
+	arr := make([]*Pure, 0, dataLen)
+	for i := 0; i < dataLen; i++ {
+		item := new(Pure)
+		item.C = a[i]
+		item.H = b[i]
+		item.L = c[i]
+		item.Range.L = i
+		item.Range.R = i
+	}
+	return arr
 }
