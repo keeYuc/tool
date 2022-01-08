96ddd31025e7b7a555d0afc0f761570601894c20
keeYuc <52500690@qq.com>
2022-01-08 17:02:53 +0800

    update

--- comment-refine/dishes.py
+++ comment-refine/dishes.py
@@ -9,10 +9,11 @@ from protocol.file_center import image_pb2
 from protocol.seo import seo_service_pb2_grpc
 from protocol.seo import data_pb2
 
-rpc_url_fct = 'localhost:9009'
-rpc_url_seo = 'localhost:9007'
-#url = 'mongodb://crawler:hha1layfqyx@gcp-docdb.cluster-cqwt9pwni8mm.ap-southeast-1.docdb.amazonaws.com:27017/?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false'
-url = 'mongodb://root:8DNsidknweoRGwSbWgDN@localhost:27019'
+rpc_url_fct = 'seo:9000'
+rpc_url_seo = 'seo:9000'
+# url = 'mongodb://crawler:hha1layfqyx@gcp-docdb.cluster-cqwt9pwni8mm.ap-southeast-1.docdb.amazonaws.com:27017/?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false'
+url = 'mongodb://crawler:hha1layfqyx@gcp-docdb.cluster-cqwt9pwni8mm.ap-southeast-1.docdb.amazonaws.com:27017/?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false'
+#url = 'mongodb://root:8DNsidknweoRGwSbWgDN@localhost:27019'
 database = "content"
 
 
