9399e018d1281c34cc109533e5e0a9c30a99be94
keeYuc <38851875+keeYuc@users.noreply.github.com>
2022-01-19 10:51:57 +0800

    Delete comment-refine directory

--- comment-refine/build.sh
+++
@@ -1,5 +0,0 @@
-python -m grpc_tools.protoc -I ./protobuf -I ./protobuf/seo --python_out=./build --grpc_python_out=./build ./protobuf/seo/data.proto
-python -m grpc_tools.protoc -I ./protobuf -I ./protobuf/seo  --python_out=./build --grpc_python_out=./build ./protobuf/seo/seo_service.proto
-#python -m grpc_tools.protoc -I ./protobuf -I ./protobuf/file-center --python_out=./protocol --grpc_python_out=./protocol ./protobuf/file-center/file_center_service.proto
-#python -m grpc_tools.protoc -I ./protobuf -I ./protobuf/file-center --python_out=./protocol --grpc_python_out=./protocol ./protobuf/file-center/image.proto
-echo "build fin"
--- comment-refine/dishes_config.py
+++
@@ -1,7 +0,0 @@
-shop_ids = ['f674eae3-adae-467c-9b89-1c59deea75e2',
-            '14b0c6a6-8219-43bb-9097-3e0e8507930b']
-
-tag_map = [('9da320da366c46aeac136d37e5ee701f',		'Piyaz', 'Piyaz'),
-           ('9da320da366c46aeac136d37e5ee701f',
-            'Ezogelin corba',	'Ezogelin corbası'),
-           ('9da320da366c46aeac136d37e5ee701f',		'Piyaz', 'Piyaz')]
