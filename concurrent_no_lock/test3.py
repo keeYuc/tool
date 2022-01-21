aa456e26d950232404b926e21e3ca79f86d2eb51
keeYuc <52500690@qq.com>
2022-01-21 11:27:33 +0800

    update

--- concurrent_no_lock/test3.py
+++ concurrent_no_lock/test3.py
@@ -1,7 +1,6 @@
 import json
-import pandas as pd
 with open('a.txt') as f:
     s = f.read()
-    #json.dump([i.strip('\n').strip(' ') for i in s.split(',')], open('b.json', 'w'))
-    pd.DataFrame([i.strip('\n').strip(' ') for i in s.split(',')]).to_csv("a.csv")
-
+    data = [i.strip('\n').strip(' ') for i in s.split(',')]
+    json.dump(data[:1000], open('b.json', 'w'))
+    #pd.DataFrame([i.strip('\n').strip(' ') for i in s.split(',')]).to_csv("a.csv")
